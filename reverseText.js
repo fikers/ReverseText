987937c74b7e0d23234667107548cb8316ba6ba3
Faique Moqeet <faiquemoqeet@gmail.com>
2017-01-24 02:46:42 -0600

    Finalized function. Added Loader CSS.

--- reverseText.js
+++ reverseText.js
@@ -1,17 +1,25 @@
-function reverseText() {
-	var text = "defines"
+function reverseText(text) {
 	var reversedText = '';
+	console.log(text);
 	var lastLetter = (text.length) - 1; 
 	for (i=lastLetter; i>=0 ; i--) {
-		console.log(i);
 		var temp = text[i];
 		console.log(temp);
 		reversedText = reversedText + temp;
 	} 
-	document.querySelector('p').innerHTML = `${reversedText}`;
-
+	reversedText = reversedText[0].toUpperCase() + reversedText.slice(1).toLowerCase()
+	console.log(reversedText)
+	document.querySelector('span').innerHTML = `${reversedText}`;
+	document.querySelector('.loader').remove();
 }
 
+
 document.addEventListener('DOMContentLoaded', function(){
-	reverseText(); 
+
+	document.addEventListener('onkeydown', function(){
+		if (e.keydown == 13) {
+			reverseText(TextArea.value);
+		}
+	})
+
 })
